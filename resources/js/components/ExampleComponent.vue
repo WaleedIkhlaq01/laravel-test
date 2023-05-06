608a5ace9fb8ba1f37dbf53173740d6a6c9e1816
saleem ayoub <saleemayoub1@gmail.com>
2023-05-06 12:31:43 +0500

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container ">
+        <div class="text-center"><h2>this is vue template</h2></div>
+    </div>
+    <div class="container text-center">
+      <h1>{{ user_prop_name }} Counter: {{ counter }}</h1>
+      <button @click="incrementCounter">Increment</button>
+      <button @click="decrementCounter">Decrement</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['initialCounter','user_name'],
+    data() {
+      return {
+        counter: 0,
+        // counter: this.initialCounter || 0,
+        // user_prop_name: 'this',
+        user_prop_name : this.user_name
+      };
+    },
+    methods: {
+      incrementCounter() {
+        this.counter++;
+      },
+      decrementCounter() {
+        this.counter--;
+      },
+    },
+  };
+  </script>
+  
